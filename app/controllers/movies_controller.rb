6faae1f3689443ec08030857b4e86bbc6add44f3
Max Bograd <mbograd@berkeley.edu>
2022-10-05 03:22:38 +0000

    modifying logic in controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,6 @@ class MoviesController < ApplicationController
     end
   
     def index
-      debugger
       if params[:ratings] != nil
         ratings = params[:ratings].keys
       elsif params["G"] != nil || params["PG"] != nil || params["PG-13"] != nil || params["R"] != nil
